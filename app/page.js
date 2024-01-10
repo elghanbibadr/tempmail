58bdd9c40fc963167f5c61187324d947517abd89
elghanbi badr <bghanbi50@gmail.com>
2024-01-10 20:30:16 +0100

    the first API call has been made successfully

--- app/page.js
+++ app/page.js
@@ -5,6 +5,7 @@ import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
 import { cookies } from 'next/headers'
 import Link from 'next/link';
 
+
 export default async function Home() {
   const cookieStore = cookies();
   const supabase = createServerComponentClient({cookies: () => cookieStore});
