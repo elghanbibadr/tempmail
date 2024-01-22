3b4a90eda27412a99ea84447071d8e29e3357e9e
elghanbibadr <bghanbi50@gmail.com>
2024-01-22 17:49:14 +0100

    storing emails to supabase

--- app/componenet/GetEmail.js
+++ app/componenet/GetEmail.js
@@ -2,6 +2,8 @@
 import { useState,useEffect } from "react";
 import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 import { useRouter } from "next/navigation";
+import Link from "next/link";
+
 const url = 'https://temp-gmail.p.rapidapi.com/get?domain=gmail.com&username=random&server=server-1&type=real';
 const options = {
 	method: 'GET',
@@ -18,12 +20,20 @@ export function Email(){
   const router = useRouter()
 
     const [tempEmail,setTempEmail]=useState(undefined)
-    const  [timestamp,setTimeStamp]=useState(undefined)
+    const [timestamp,setTimeStamp]=useState(undefined)
     const [isLoading,setIsLoading]=useState(false)
     const [User,setUser]=useState(undefined)
+    const [emailSelected,setEmailSelected]=useState(false)
+    const [userGeneratedEmails,setUserGeneratedEmails]=useState([])
+    const [currentViewedEmail,setCurrentViewedEmail]=useState("")
+    const [currentViewedEmailTimeStamp,setCurrentViewedEmailTimeStamp]=useState("")
+    const [emailData, setEmailData] = useState(null);
+
     // const [receivedMessage,setReceivedMessage]=useState(null)
     const supabase = createClientComponentClient();
 
+
+    // LOADING THE USER FROM SUPABASE
     useEffect(() => {
       async function getUser(){
           const {data: {user}} = await supabase.auth.getUser()
@@ -33,11 +43,16 @@ export function Email(){
       getUser();
   }, [])
 
-        const [emailData, setEmailData] = useState(null);
-        const emailurl = `https://temp-gmail.p.rapidapi.com/check?email=${tempEmail}&timestamp=${timestamp}`;
-
+    
+  // FETCH THE INBOXES INSIDE THE CURRENTLY VIEWED EMAIL 
         const fetchEmailData = async () => {
+          console.log("temp email", tempEmail)
+          console.log("current email", currentViewedEmail)
+          const emailurl = `https://temp-gmail.p.rapidapi.com/check?email=${currentViewedEmail}&timestamp=${currentViewedEmailTimeStamp}`;
+       
+
           try { 
+          //getting the email inboxes
             const response = await fetch(emailurl, options);
             if (!response.ok) {
               throw new Error('Network request failed');
@@ -45,77 +60,101 @@ export function Email(){
           
             const data = await response.json();
             setEmailData(data);
-            console.log(data)
+            // if (data.items.length !==0)
+            console.log('email data', data)
+          //storing email data to supabase 
+          if (data.items && data.items.length > 0) {
+            console.log(data.items[0].textFrom);      
+           await supabase
+          .from('emailInboxes')
+          .insert([
+          {textFrom:data.items[0].textFrom,textTo:data.items[0].textTo,textDate:data.items[0].textDate}
+          ])
+          .select()
+        }
           } catch (error) {
+             alert('this email has been destroyed')
             console.error('Error fetching data:', error);
           }
-        };
+        }
+      
       
 
-        // setInterval(() => {
-        //     fetchEmailData()
-        // }, 1000);
+    
        
-const getEmail=async () =>{
-  console.log('current tempmail is ' ,tempEmail)
-  console.log('current user id is ' ,User?.id)
- 
-
-    // getStoredUserEmails()
-    try {
-      setIsLoading(true)
-      const response = await fetch(url, options);
-      const result = await response.json();
-      // console.log(result["items"])
-      console.log(result);
-      setIsLoading(false)
-      if(result.msg ===  'OK'){
-        setTempEmail(result.items.email)
-        setTimeStamp(result.items.timestamp)
-  
-      }
-    } catch (error) {
-      console.error(error);
-    }
-  }
-
-  
-  useEffect( () =>{
-
-  
-     return () => getEmail()
-
-  },[])
-
-
- async function getStoredUserEmails() {
-    console.log(tempEmail)
-  if(!tempEmail) return
-    const { data, error } = 
-    await supabase
-    .from('emailsList')
-    .insert([
-      { userIdEmail: User?.id,emailText: tempEmail },
-    ])
-    .select()
-  }
-
-  useEffect(  () =>{
-    console.log(User.id)
-    console.log(tempEmail)
-    const addNew=async () =>{
+  console.log(currentViewedEmail)
+  const addNew=async () =>{
     const {data,error}= await supabase
     .from('emailsList')
     .insert([
-      {userIdEmail:"HDJSGJGF6484",emailText:tempEmail},
+      {userIdEmail:User?.id,emailText:tempEmail,timestamp:timestamp},
     ])
     .select()   
     console.log(data)
   }
- 
 
-  addNew()
-  },[tempEmail])
+
+
+  console.log("email data", emailData)
+  // GETT A NEW EMAIL ADDRESS
+// const getEmail=async () =>{
+//     try {
+//       setIsLoading(true)
+//       const response = await fetch(url, options);
+//       const result = await response.json();
+//       // console.log(result["items"])
+//       console.log(result);
+//       setIsLoading(false)
+//       if(result.msg ===  'OK'){
+//         setTempEmail(result.items.email)
+//         setTimeStamp(result.items.timestamp)
+  
+//       }
+//     } catch (error) {
+//       console.error(error);
+//     }
+//   }
+
+const getEmail = async () => {
+  try {
+    setIsLoading(true);
+    const response = await fetch(url, options);
+    const result = await response.json();
+    setIsLoading(false);
+
+    if (result.msg === 'OK') {
+      const generatedEmail = result.items.email;
+      const timestamp = result.items.timestamp;
+    addNew()
+    allData()
+      // Store the generated email and timestamp in local storage
+      localStorage.setItem('generatedEmail', generatedEmail);
+      localStorage.setItem('timestamp', timestamp);
+      
+      setTempEmail(generatedEmail);
+      setTimeStamp(timestamp);
+      // add the new email to the array of generated email 
+      
+      const existingGeneratedEmails = JSON.parse(localStorage.getItem('generatedEmailsList')) || [];
+      existingGeneratedEmails.push({email:tempEmail,timestamp:timestamp})
+
+        
+
+    }
+  } catch (error) {
+    console.error(error);
+  }
+}
+  
+
+
+console.log(emailData)
+
+  // GETTING A NEW EMAIL WHENEVER THE COMPONENET MOUNT
+  // useEffect( () =>{
+  //    return () => getEmail()
+  // },[])
+
 
 
 console.log(User?.id)
@@ -125,24 +164,54 @@ console.log(User?.id)
     // setUser(null)
 }
 
-useEffect(() =>{
-  const allData=async () =>{
-let { data: emailsList, error } = await supabase
-.from('emailsList')
-.select('*')
-console.log(emailsList)
-  }
 
-  allData()
-},[])
-  
+const allData=async () =>{
+  let { data: emailsList, error } = await supabase
+  .from('emailsList')
+  .select().eq('userIdEmail',User?.id)
+  setUserGeneratedEmails(emailsList)
+  console.log(emailsList)
+}
+
+
+useEffect( () =>{
+ allData()
+},[User])
+// allData()
+
+
+
+
+const handleEmailSelected=(selectedEmail,selectedEmailTimeStamp) =>{
+ setEmailSelected(true)
+ setCurrentViewedEmail(selectedEmail)
+ setCurrentViewedEmailTimeStamp(selectedEmailTimeStamp)
+ console.log(selectedEmailTimeStamp)
+}
+
+
+// TEST FETCHES
+
+  // fetch(`https://temp-gmail.p.rapidapi.com/check?email=mallinakwalthalladxis68@gmail.com&timestamp=1705506058`,options).then(res=>res.json()).then(data=>console.log(data))
+// Retrieve stored values from local storage on page load
+useEffect(() => {
+  const storedGeneratedEmail = localStorage.getItem('generatedEmail');
+  const storedTimestamp = localStorage.getItem('timestamp');
+
+  if (storedGeneratedEmail && storedTimestamp) {
+    setTempEmail(storedGeneratedEmail);
+    setTimeStamp(storedTimestamp);
+  }
+}, []);
+
+
+
     return <>
-    
     <div className='border max-w-[900px] mx-auto border-accent1 border-dashed shadow-pinkBoxShadow2 p-6 rounded-xl mt-10'>
       <button className="bg-darkPink  p-2 rounded-md" onClick={handleLogout}>logout</button>
       <h2 className='text-2xl text-center mb-10 mt-4'>Your Temporary Email Address</h2>
       <div className="rounded-full p-4 px-6 bg-darkPink my-4">
-        {!isLoading &&  <p>{tempEmail}</p>}
+        {!isLoading &&  <p onClick={() => handleEmailSelected(tempEmail,timestamp)}>{tempEmail}</p>}
         {isLoading &&  <p>loading ...</p>}
       </div>
       
@@ -161,40 +230,53 @@ console.log(emailsList)
           <p>refresh</p>
         </div> */}
       </div>
-      {/* <div className="max-w-[900px] mx-auto  my-4  shadow-pinkBoxShadow2 rounded-xl mt-10">
-      <div className="bg-darkPink flex rounded-t-lg justify-between py-2 mb-10">
-        <div className='py-2 rounded-full px-5 text-sm'>
-            <h4 >SENDER</h4>
-          </div>
-          <div className='py-2 rounded-full px-5 text-sm'>
-            <h4>SUBJECT</h4>
-          </div>
-          <div className='py-2 rounded-full px-5 text-sm'>
-            <h4>DATE</h4>
-          </div>
-      </div>
-      <div className="p-3">
+   
+      <div className="max-w-[900px] mx-auto px-6 my-4 n shadow-pinkBoxShadow2 p-6 rounded-xl mt-10" >
+        <h3 className="text-center mb-10 uppercase text-red-600">your generated emails</h3>
+        { userGeneratedEmails && userGeneratedEmails.map((item) =>{
+          return  <h2 onClick={() => handleEmailSelected(item.emailText,item.timestamp)} key={item.id} className="my-3 cursor-pointer">{item.emailText}</h2>
         
-        {emailData?.items.length === 0 && <p className="text-center mb-6">Your inbox is empty
+        } )}
 
-Waiting for incoming emails</p>}
-
-{emailData && emailData.items.length > 0 &&  emailData.items.map((item) =>{
-  {console.log(item)}
-  return <ul className="flex justify-between mb-6">
-  { <li>{item?.textFrom}</li> }
-  { <li>{item?.textSubject}</li> }
-  { <li>{item?.textDate}</li> }
-</ul>
-}) 
- 
- }
-<button className="bg-darkPink px-10 py-2 mt-6 rounded-md mb-6 mx-auto block" onClick={fetchEmailData}>refresh</button>
-
+        
       </div>
-      </div> */}
-      <div className="max-w-[900px] mx-auto px-6 my-4 flex justify-between shadow-pinkBoxShadow2 p-6 rounded-xl mt-10" >
-        <h3>your generated emails</h3>
-      </div>
+
+         {emailSelected && <div className="bg-[#333] fixed h-screen w-screen inset-0 flex items-center ">
+         <h2 className="cursor-pointer" onClick={() => setEmailSelected(false)}>back </h2>
+            <div className=" mx-auto  my-4 w-[80%]  shadow-pinkBoxShadow2 rounded-xl mt-10">
+              <h2 className="text-white">{currentViewedEmail}</h2>
+                  <div className="bg-darkPink flex rounded-t-lg justify-between py-2 mb-10">
+                    <div className='py-2 rounded-full px-5 text-sm'>
+              <h4 >SENDER</h4>
+            </div>
+            <div className='py-2 rounded-full px-5 text-sm'>
+              <h4>SUBJECT</h4>
+            </div>
+            <div className='py-2 rounded-full px-5 text-sm'>
+              <h4>DATE</h4>
+            </div>
+                  </div>
+                  <div className="p-3">
+                    
+                    {emailData?.items.length === 0 && <p className="text-center mb-6">Your inbox is empty
+            
+            Waiting for incoming emails</p>}
+            
+            {emailData && emailData.items.length > 0 &&  emailData.items.map((item,index) =>{
+              {console.log(item)}
+              return <ul key={index} className="flex justify-between mb-6">
+              { <li>{item?.textFrom}</li> }
+              { <li>{item?.textSubject}</li> }
+              { <li>{item?.textDate}</li> }
+            </ul>
+            }) 
+             
+             }
+            <button className="bg-darkPink px-10 py-2 mt-6 rounded-md mb-6 mx-auto block" onClick={fetchEmailData}>refresh</button>
+            
+                  </div>
+                  </div>
+          </div>}
+
     </>
 }
