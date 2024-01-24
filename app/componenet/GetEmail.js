262e9846c9c32e04f115c3814bc0712ff36d153e
elghanbibadr <bghanbi50@gmail.com>
2024-01-24 16:02:37 +0100

    more troubleshooting

--- app/componenet/GetEmail.js
+++ app/componenet/GetEmail.js
@@ -3,7 +3,7 @@ import { useState,useEffect } from "react";
 import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 import { useRouter } from "next/navigation";
 import Link from "next/link";
-
+import Image from 'next/image'
 const url = 'https://temp-gmail.p.rapidapi.com/get?domain=gmail.com&username=random&server=server-1&type=real';
 const options = {
 	method: 'GET',
@@ -29,7 +29,8 @@ export  function Email(){
     const [currentViewedEmailTimeStamp,setCurrentViewedEmailTimeStamp]=useState("")
     const [emailData, setEmailData] = useState(null);
     const [isLoadingPreviousGeneratedEmails, setIsLoadingPreviousGeneratedEmails]=useState(true)
-
+    const [userDeleteEmail,setUserDeleteEmail]=useState(false)
+    const [isLoadingEmailInbox,setIsLoadingEmailInbox]=useState(false)
     // const [receivedMessage,setReceivedMessage]=useState(null)
     const supabase = createClientComponentClient();
 
@@ -46,11 +47,8 @@ export  function Email(){
 
     
   // FETCH THE INBOXES INSIDE THE CURRENTLY VIEWED EMAIL 
-        const fetchEmailData = async () => {
-        
+        const fetchEmailData = async () => {        
           const emailurl = `https://temp-gmail.p.rapidapi.com/check?email=${currentViewedEmail}&timestamp=${currentViewedEmailTimeStamp}`;
-       
-
           try { 
           //getting the email inboxes
             const response = await fetch(emailurl, options);
@@ -63,12 +61,14 @@ export  function Email(){
              .select('*')
              .eq('textTo',currentViewedEmail)
             // console.log(emailInboxes)
-             setEmailData({items:emailInboxes})
+            let emailInboxesFilter = emailInboxes.filter((item, index, array) => {
+              // Keep only the first occurrence of each textSubject
+              return array.findIndex((el) => el.textSubject === item.textSubject) === index;
+            });
+             setEmailData({items:emailInboxesFilter})
+             console.log( 'duplicated removed' , emailInboxesFilter)
              return 
-            }
-          
-
-            
+            }                    
             const data = await response.json();
             setEmailData(data);
             // if (data.items.length !==0)
@@ -86,6 +86,8 @@ export  function Email(){
           } catch (error) {
              alert('this email has been destroyed')
             console.error('Error fetching data:', error);
+          }finally{
+            setIsLoadingEmailInbox(false)
           }
         }
       
@@ -103,9 +105,6 @@ export  function Email(){
     .select()   
     console.log(data)
   }
-
-
-
 
 
 const getEmail = async () => {
@@ -135,6 +134,24 @@ const getEmail = async () => {
   
 
 
+const handleEmailDeleted=async (itemId) => {
+  setUserDeleteEmail(true)
+  console.log(itemId)
+  try {
+    const { data, error } = await supabase
+      .from("emailsList")
+      .delete()
+      .eq("id",itemId);
+
+      if (error) {
+      console.error('Error deleting item:', error.message);
+      } else {
+       console.log('Item deleted successfully:', data);
+       }
+  } catch (e) {
+    console.log('error')
+}
+}
 
 
 console.log(User?.id)
@@ -175,15 +192,16 @@ useEffect(() => {
 
 const handleEmailSelected=(selectedEmail,selectedEmailTimeStamp) =>{
   setEmailData(null)
+  setIsLoadingEmailInbox(true)
  setEmailSelected(true)
  setCurrentViewedEmail(selectedEmail)
  setCurrentViewedEmailTimeStamp(selectedEmailTimeStamp)
  fetchEmailData()
+
 }
 
 
 // TEST FETCHES
-
   // fetch(`https://temp-gmail.p.rapidapi.com/check?email=mallinakwalthalladxis68@gmail.com&timestamp=1705506058`,options).then(res=>res.json()).then(data=>console.log(data))
 // Retrieve stored values from local storage on page load
 useEffect(() => {
@@ -198,80 +216,84 @@ useEffect(() => {
 
 
 
-    return <>
-    <div className='border max-w-[900px] mx-auto border-accent1 border-dashed shadow-pinkBoxShadow2 p-6 rounded-xl mt-10'>
-      <button className="bg-darkPink  p-2 rounded-md" onClick={handleLogout}>logout</button>
+    return <div className="max-w-[900px] mx-auto">
+      <div className="flex justify-between items-center">
+      <h3 className="text-xl">Temp Mail</h3>
+        <button className="bg-darkPink  p-2 px-8 rounded-md" onClick={handleLogout}>logout</button>
+      </div>
+    <div className='border  border-accent1 border-dashed shadow-pinkBoxShadow2 p-6 rounded-xl mt-10'>
       <h2 className='text-2xl text-center mb-10 mt-4'>Your Temporary Email Address</h2>
-      <div className="rounded-full p-4 px-6 bg-darkPink my-4">
+      <div className="rounded-full flex justify-between p-4 px-6 bg-darkPink my-4">
         {!isLoading &&  <p onClick={() => handleEmailSelected(tempEmail,timestamp)}>{tempEmail}</p>}
         {isLoading &&  <p>loading ...</p>}
-      </div>
-      
-      </div>
-      <div className="max-w-[900px] mx-auto px-6 my-4 flex justify-between shadow-pinkBoxShadow2 p-6 rounded-xl mt-10">
-        <div className='bg-darkPink py-2 rounded-full px-5 text-sm'>
-          <button onClick={getEmail}>refresh</button>
-        </div>
-        <div className='bg-darkPink py-2 rounded-full px-5 text-sm'>
-          <button onClick={getEmail}>delete</button>
-        </div>
-        <div className='bg-darkPink py-2 rounded-full px-5 text-sm'>
-          <button onClick={getEmail}>change</button>
-        </div>
-        {/* <div>
-          <p>refresh</p>
-        </div> */}
-      </div>
-   
-      <div className="max-w-[900px] mx-auto px-6 my-4 n shadow-pinkBoxShadow2 p-6 rounded-xl mt-10" >
+        <svg onClick={getEmail} className="cursor-pointer" fill="white"  xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 30 30" width="22px" height="22px"><path d="M 15 3 C 12.031398 3 9.3028202 4.0834384 7.2070312 5.875 A 1.0001 1.0001 0 1 0 8.5058594 7.3945312 C 10.25407 5.9000929 12.516602 5 15 5 C 20.19656 5 24.450989 8.9379267 24.951172 14 L 22 14 L 26 20 L 30 14 L 26.949219 14 C 26.437925 7.8516588 21.277839 3 15 3 z M 4 10 L 0 16 L 3.0507812 16 C 3.562075 22.148341 8.7221607 27 15 27 C 17.968602 27 20.69718 25.916562 22.792969 24.125 A 1.0001 1.0001 0 1 0 21.494141 22.605469 C 19.74593 24.099907 17.483398 25 15 25 C 9.80344 25 5.5490109 21.062074 5.0488281 16 L 8 16 L 4 10 z"/></svg>
+      </div>      
+      </div>   
+      <div className=" px-6 my-4 n shadow-pinkBoxShadow2 p-6 rounded-xl mt-10" >
         <h3 className="text-center mb-10 uppercase text-white">Your Generated Emails</h3>
         {userGeneratedEmails.length === 0 && isLoadingPreviousGeneratedEmails && <p className="text-white text-base text-center ">Getting Your previous email ... </p>}
         {userGeneratedEmails.length === 0 && !isLoadingPreviousGeneratedEmails && <p className="text-white text-base text-center capitalize "> You has not genrated any email </p>}
 
         { userGeneratedEmails && userGeneratedEmails.map((item) =>{
-          return  <h2 onClick={() => handleEmailSelected(item.emailText,item.timestamp)} key={item.id} className="my-3 cursor-pointer">{item.emailText}</h2>
+          return  <div className="flex items-center justify-between" >
+                       
+                 <h2 onClick={() => handleEmailSelected(item.emailText,item.timestamp)} key={item.id} className="my-3 cursor-pointer">{item.emailText}</h2>
+                 <Image
+                
+                onClick={() => handleEmailDeleted(item.id)}
+      src="/delete.svg"
+      width={14}
+      height={14}
+      className="cursor-pointer"
+      alt="Picture of the author"
+    />
+                
+                </div>
         
-        } )}
-
-        
+        } )}        
       </div>
 
-         {emailSelected && <div className="bg-[#333] fixed h-screen w-screen inset-0 flex items-center ">
-         <h2 className="cursor-pointer" onClick={() => setEmailSelected(false)}>back </h2>
-            <div className=" mx-auto  my-4 w-[80%]  shadow-pinkBoxShadow2 rounded-xl mt-10">
-              <h2 className="text-white">{currentViewedEmail}</h2>
-                  <div className="bg-darkPink flex rounded-t-lg justify-between py-2 mb-10">
-                    <div className='py-2 rounded-full px-5 text-sm'>
-              <h4 >SENDER</h4>
-            </div>
-            <div className='py-2 rounded-full px-5 text-sm'>
-              <h4>SUBJECT</h4>
-            </div>
-            <div className='py-2 rounded-full px-5 text-sm'>
-              <h4>DATE</h4>
-            </div>
-                  </div>
-                  <div className="p-3">
-                    
-                    {emailData?.items.length === 0 && <p className="text-center mb-6">Your inbox is empty
-            
-            Waiting for incoming emails</p>}
-            
-            {emailData && emailData.items.length > 0 &&  emailData.items.map((item,index) =>{
-              {console.log(item)}
-              return <ul key={index} className="flex justify-between mb-6">
-              { <li>{item?.textFrom}</li> }
-              { <li>{item?.textSubject}</li> }
-              { <li>{item?.textDate}</li> }
-            </ul>
-            }) 
-             
-             }
-            <button className="bg-darkPink px-10 py-2 mt-6 rounded-md mb-6 mx-auto block" onClick={fetchEmailData}>refresh</button>
-            
-                  </div>
-                  </div>
-          </div>}
+         <div className="">
+         
+           {emailSelected && <div className="bg-[#333] fixed h-screen w-screen inset-0 flex flex-col items-center ">
+           <div onClick={() => setEmailSelected(false)} className="flex cursor-pointer self-start justify-self-start mt-4">
+             <svg  className="h-6 rotate-180 mx-2" fill="white" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 25 25"><title>Artboard-34</title><g id="Right-2" data-name="Right"><polygon points="17.5 5.999 16.793 6.706 22.086 11.999 1 11.999 1 12.999 22.086 12.999 16.792 18.294 17.499 19.001 24 12.499 17.5 5.999" /></g></svg>
+             <h2>Back</h2>
+              
+           </div>
+              <div className=" mx-auto  my-4 w-[80%]  shadow-pinkBoxShadow2 rounded-xl mt-10">
+                {/* <h2 className="text-white">{currentViewedEmail}</h2> */}
+                    <div className="bg-darkPink flex rounded-t-lg justify-between py-2 mb-10">
+                      <div className='py-2 rounded-full px-5 text-sm'>
+                <h4 >SENDER</h4>
+              </div>
+              <div className='py-2 rounded-full px-5 text-sm'>
+                <h4>SUBJECT</h4>
+              </div>
+              <div className='py-2 rounded-full px-5 text-sm'>
+                <h4>DATE</h4>
+              </div>
+                    </div>
+                    <div className="p-3">
+                      {isLoadingEmailInbox && <p className="text-white text-xl">loading ...</p>}
+                      {emailData?.items.length === 0 && !isLoadingEmailInbox && <p className="text-center mb-6">Your inbox is empty Waiting for incoming emails</p>}
+           
+              {emailData && emailData.items.length > 0 &&  emailData.items.map((item,index) =>{
+                {console.log(item)}
+                return <ul key={index} className="flex justify-between mb-6">
+                { <li>{item?.textFrom}</li> }
+                { <li>{item?.textSubject}</li> }
+                { <li>{item?.textDate}</li> }
+              </ul>
+              })
+           
+               }
+              <button className="bg-darkPink px-10 py-2 mt-6 rounded-md mb-6 mx-auto block" onClick={fetchEmailData}>refresh</button>
+           
+                    </div>
+                    </div>
+            </div>}
+         </div>
 
-    </>
+    </div>
 }
